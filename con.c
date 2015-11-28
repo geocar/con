ed81637c3aa6fc06d44fa5278b2845b87cf42243
Geo Carncross <geocar@gmail.com>
2015-11-28 14:32:50 +0000

    line editing

--- con.c
+++ con.c
@@ -16,24 +16,35 @@ ZI L='q';ZS l[]={
   "q){[x];a:.q.show;b:neg .z.w;.q.show::{x .Q.s y;}[b];@[{.q.show value x;};x;{x\"'\",y,\"\\n\"}[b]];.q.show::a;}",
   "k){a:.q.show;b:-.z.w;.q.show::{x .Q.s y;}[b];@[{x (-3!.:y),\"\\n\"}[b];x;{x\"'\",y,\"\\n\"}[b]];.q.show::a;}"
 };
+ZS sd(S p,I n){if(n>=10)p=sd(p,n/10);*p='0'+(n%10);R p+1;}
+ZI sw(V){struct winsize ws={0};ioctl(0,TIOCGWINSZ,&ws);if(!ws.ws_col)ioctl(1,TIOCGWINSZ,&ws);R ws.ws_col;}
 ZI i0(V){C b[4];if(read(0,b,1)==1)R *b;R -1;}
 ZI ox(I f,S x,I n){I r;while(n>0)if((r=write(1,x,n))<=0)break;else x+=r,n-=r;R 1;}ZI o1(S x,I n){ox(1,x,n);}ZI o1c(I c){C b[4]={c,0,0,0};R o1(b,1);}
-ZI oxs(I f,S x){R ox(f,x,strlen(x));}ZI o1s(S x){R oxs(1,x);}ZI o1r(V){R o1s("\r\x1b[K");}
-ZV rp(K x){if('k'==L)o1s("  ");else o1(xC,2);}ZK nk(V){K x=ktn(KC,2);kC(x)[1]=')';kC(x)[0]=L;rp(x);R x;}ZV rk(K x){o1r();rp(x);o1(xC+2,xn-2);}
+ZI oxs(I f,S x){R ox(f,x,strlen(x));}ZI o1s(S x){R oxs(1,x);}ZI mc(I y,I x){C*p,b[99]={27,'['};p=sd(b+2,y);*p=';';p=sd(p+1,x);*p='H';p[1]=0;o1s(b);}
+ZV rp(K x){o1s("\x1b[0J");if('k'==L)o1s("  ");else o1(xC,2);}
+ZK nk(I cr){K x=ktn(KC,2);kC(x)[1]=')';kC(x)[0]=L;if(cr)o1s("\r\n");o1s("\x1b[6n");R x;}
 
-int main(int argc,char *argv[]){I f,c,d=0;C b[4096];K x,y;struct pollfd p[2]={0};struct termios G0,G1;
+//position fd(kdb) char displaymode
+int main(int argc,char *argv[]){I Y,X,p,f,c,d=0;C b[4096];K x,y;struct pollfd P[2]={0};struct termios G0,G1;
   if(argc == 1){oxs(2,"Usage: ");oxs(2,*argv);oxs(2," [host] port\n");R 1;}
   if(-1==(f=khpu(argc==2?"127.0.0.1":argv[1],atoi(argv[argc-1]),getenv("USER")))){oxs(2,"kdb: ");oxs(2,strerror(errno));oxs(2,"\n");R 2;}
   tcgetattr(0,&G0);G1=G0;G1.c_iflag&=~(BRKINT|ICRNL|INPCK|ISTRIP|IXON);G1.c_oflag&=~OPOST;G1.c_cflag|=CS8;G1.c_lflag&=~(ECHO|ICANON|IEXTEN|ISIG);G1.c_cc[VMIN]=1;G1.c_cc[VTIME]=0;
   tcsetattr(0,TCSAFLUSH,&G1);
 
-  p->fd=0;p[1].fd=f;DO(2,p[i].events=POLLIN|POLLHUP);
-  x=nk();while(-1!=poll(p,2,-1))
-    if(p[0].revents&POLLIN)switch(c=i0()){
-      case 3:case 4:case -1:shutdown(f,SHUT_WR);break;case 12:rk(x),d=0;break;case 8:case 127:if(xn>2)o1s("\b \b"),xn--;break;
-      case '\r':case '\n':c=(xn==3&&xC[2]=='\\');if(!c&&xn>2)k(-f,l[L=='k'],x,0,0);else r0(x); if(c)L=*l[L!='k']; o1s("\r\n");x=nk();break;
-      default:ja(&x,&c);/*littleendian*/o1c(c);break;
-    } else if(p[1].revents&POLLIN){if(0>=(c=read(f,b,4096)))break;if(!d)o1r();d=1;o1(b,c);if(b[c-1]=='\n')rk(x),d=0;}
+  P->fd=0;P[1].fd=f;DO(2,P[i].events=POLLIN|POLLHUP);
+  Y=X=0;x=nk(0);p=2;while(-1!=poll(P,2,-1))
+    if(P[0].revents&POLLIN)switch(c=i0()){
+      case 3:case -1:shutdown(f,SHUT_WR);break;
+      case 12:redraw:mc(Y,X);rp(x);o1(xC+2,xn-2);d=0;gotoxy:if(p!=xn){I w=sw();if(w>0)mc(Y+(p/w),X+(p%w));else{mc(Y,X);rp(x);o1(xC+2,p-2);}}break;
+      case 8:case 127:if(p>2){if(p==xn)o1s("\b \b"),p--,--xn;else{o1s("\x1b[D");o1(xC+p,xn-p);o1c(' ');--p;goto del;}}break;//delete
+      case 11:xn=p;o1s("\x1b[0J");break;case 4:del:if(p==xn)break;memmove(xC+p,xC+p+1,xn-(p+1));xn--;o1(xC+p,xn-p);if(xn==p)o1s(" \b");else{o1c(' ');goto gotoxy;}
+      case 1:p=2;mc(Y,X);o1s("\x1b[2C");break;case 2:back:if(p>2){--p;o1s("\x1b[D");}break;case 6:forward:if(p<xn){++p;o1s("\x1b[C");}break;
+      case 27:if('['==(c=i0())){I n[5]={0},j=0;do{while((c=i0())>='0'&&c<='9'&&j<5)n[j]=(c-'0')+(n[j]*10);++j;}while(c==';');switch(c){
+        case 'R':Y=n[0],X=n[1];goto redraw;case 'D':goto back;case 'C':goto forward;case '~':if(n[0]==3)goto del;
+      }};break;
+      case '\r':case '\n':c=(xn==3&&xC[2]=='\\');if(!c&&xn>2)k(-f,l[L=='k'],x,0,0);else r0(x); if(c)L=*l[L!='k']; x=nk(1);p=2;break;
+      default:if(p==xn){ja(&x,b);xC[p]=c;o1c(c);++p;break;}ja(&x,b);memmove(xC+p+1,xC+p,xn-(p+1));xC[p]=c;o1(xC+p,1+(xn-p));++p;goto gotoxy;
+    } else if(P[1].revents&POLLIN){if(0>=(c=read(f,b,4096)))break;if(!d)mc(Y,X);d=1;o1(b,c);if(b[c-1]=='\n')goto redraw;}
   tcsetattr(0,TCSAFLUSH,&G0);
   R 0;
 }
